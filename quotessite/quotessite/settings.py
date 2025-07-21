cefccfb61331494e573518dad85f392d80f273b8
TheComedian <andyhoma69@gmail.com>
2025-07-21 19:07:15 +0300

    Final

--- quotessite/quotessite/settings.py
+++ quotessite/quotessite/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "main" / "static",
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
